43db15cba9f8af32577557a667e1c576626d91f4
rajneeshsingh1007@gmail.com <rajneeshsingh1007@gmail.com>
2022-01-15 11:41:58 +0530

    added json dynamic form validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useReducer} from 'react';
+import {TextField, Button, Typography, Grid, Card, CardContent } from '@material-ui/core'
+import { InputFormElements } from './formElement';
 
-function App() {
+
+
+const App = () => {
+  const [formInput, SetFormInput] = useReducer((state, newState) => ({ ...state, ...newState }), {
+    firstName: 'aman'
+  })
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const data = { formInput }
+    console.log('data', data)
+  }
+
+  const handleInput = (e) => {
+    const name= e.target.name
+    const newValue = e.target.value
+    SetFormInput({[name]: newValue})
+    console.log('dat', {[name]: newValue})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center'}}>
+      <Card
+        style={{
+          height: "auto",
+          width: '600px',
+          border: "1px solid grey",
+          background: "yellow"
+        }}
+      >
+        <CardContent>
+          <Typography align='center' variant='h4'>Create An Account</Typography>
+          <Typography align='center' variant='h6'>Fill all the Mandatory Fields to Create an Account</Typography>
+          <form onSubmit={submitHandler}>
+            <Typography align='left' style={{margin: "5px 0px"}}>Personal Info:</Typography>
+            <Grid container spacing={3} style={{ padding: "0px"}}>
+              {InputFormElements.slice(0, 4).map((input) => {
+                return (
+              <Grid item xs={input.xs} sm={input.sm}>
+                <TextField {...input} onChange={handleInput} />
+              </Grid>
+                )
+              } )}
+            </Grid>
+            <Typography align='left' style={{margin: "5px 0px"}}>Address Info:</Typography>
+            <Grid container spacing={3} style={{ padding: "0px" }}>
+              {InputFormElements.slice(4, 10).map((input) => {
+                return (
+              <Grid item xs={input.xs} sm={input.sm}>
+                <TextField {...input} onChange={handleInput} />
+              </Grid>
+                )
+              } )}
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} align='right'>
+                <Button type='reset' color='primary' variant='outlined' style={{margin: "0px 5px"}}>
+                  Reset
+                </Button>
+                <Button type='submit' color='primary' variant='contained'>
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/formElement.jsx
@@ -0,0 +1,38 @@
+export const InputFormElements = [
+    {
+        name:'fistName' ,placeholder:'Enter first Name', label: 'First Name', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'lastName' ,placeholder:'Enter last Name', label: 'Last Name', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'email' ,placeholder:'Email', label: 'Email', variant:'outlined', required:true,
+        type:"email", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'mobileno' ,placeholder:'Enter Mobile no', label: 'Mobile No', variant:'outlined', required:true,
+        type:"number", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'street' ,placeholder:'Enter street name', label: 'Street Name', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'city' ,placeholder:'Enter city Name', label: 'City', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'state' ,placeholder:'Enter State name', label: 'State', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'country' ,placeholder:'Enter Country Name', label: 'Country', variant:'outlined', required:true,
+        type:"text", xs:"12", sm:'6', fullWidth: true
+    },
+    {
+        name:'pincode' ,placeholder:'Enter Pincode', label: 'PinCode', variant:'outlined', required:true,
+        type:"number", xs:"12", sm:'6', fullWidth: true
+}
+]
